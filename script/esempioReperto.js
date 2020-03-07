ff164d37d78b00448f5707f128f706efd704bd95
Giuseppe Steduto <34663336+giuseppe-steduto@users.noreply.github.com>
2020-03-08 00:34:15 +0100

    Fetch lista di reperti in base alla query
    File JavaScript che prende una keyword da un campo di inserimento, fa il fetch della lista dei reperti che combaciano con quella stringa e crea dei nodi per visualizzarli nella pagina HTML. Il file fa riferimento a "esempioReperto.php" e "esempioReperto.css".

---
+++ script/esempioReperto.js
@@ -0,0 +1,16 @@
+var reperto = {
+    codassoluto:032,
+    codrelativo:015,
+    sezione:"E",
+    datainiziouso:1980,
+    datafineuso:2010,
+    descrizione:"Questa è la descrizione. È di un reperto esempio, nulla di che.",
+    autori:["BTicinoSpa", "Altro"],
+    misure:{
+            lunghezza:25,
+            larghezza:30,
+            profondita:50,
+            memoria:100
+        },
+    materiali:["Plastica", "Ferro"]
+};
