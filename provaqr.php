20bebe671c40fc4ac53828a342262516541ee88d
Giuseppe Bocci <61885676+GiuseppeBocci@users.noreply.github.com>
2020-03-11 14:27:05 +0100

    Add files via upload

---
+++ provaqr.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel = "stylesheet" href="./allstyles.css" />
+        <link rel = "manifest" href = "manifest.json" />
+        <script src = "elencoReperti.js"></script>
+        <title>MIIDM App</title>
+    </head>
+    <body>
+        <?php include("navbar.html"); ?>
+        <div id = "page_render">
+            <?php include("qrscanner.html"); ?>
+        </div>
+        <?php include("bottom-navbar.html"); ?>
+        <script src = "page_navigation.js"></script>
+    </body>
+</html>
